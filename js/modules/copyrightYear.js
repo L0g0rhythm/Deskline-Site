60ceebd26dfe0c08f6a3e168411b6133d0b1fa75
Vernier <114192446+L0g0rhythm@users.noreply.github.com>
2025-04-21 20:05:54 -0300

    Delete js directory

--- js/modules/copyrightYear.js
+++
@@ -1,9 +0,0 @@
-// js/modules/copyrightYear.js
-export function updateCopyrightYear() {
-  const yearSpan = document.getElementById("current-year");
-  if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
-  } else {
-    console.warn("Element with ID 'current-year' not found.");
-  }
-}
